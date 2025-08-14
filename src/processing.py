b377699d01d67df1ba447b9ea35ac65e5e5a8e1c
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-08-14 19:01:17 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     """
     Фильтрует список словарей по значению ключа 'state'.
@@ -11,3 +13,23 @@ def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     """
     # Используем list comprehension для фильтрации
     return [item for item in data if item.get('state') == state]
+
+
+def sort_by_date(data, descending=True):
+    """
+    Сортирует список словарей по дате в порядке убывания или возрастания.
+
+    Параметры:
+    data (list): список словарей с записями
+    descending (bool): True - сортировка по убыванию, False - по возрастанию
+
+    Возвращает:
+    list: отсортированный список словарей
+    """
+    # Сортируем список с использованием лямбда-функции и заданного порядка
+    sorted_data = sorted(
+        data,
+        key=lambda item: datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+        reverse=descending
+    )
+    return sorted_data
