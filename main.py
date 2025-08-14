40cf5763cb5a073396e5f318afc659c3fc0606fb
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-08-14 20:17:02 +0300

    add 3 DZ code+ to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state, sort_by_date
 
 # Домашнее задание 9.1
 print(get_mask_card_number(1234567890123456))
