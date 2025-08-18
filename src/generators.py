80ab5528b3082f06ea85df33456fc383b718e1c2
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-08-18 21:19:09 +0300

    wh_11_1 add tests for def card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,18 @@
 from typing import Generator
 
-def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
+def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
     """Генератор последовательности номеров карт"""
 
     # Проверяем корректность входных данных
     if not (1 <= start <= 9999999999999999):
         raise ValueError("Начальное значение должно быть от 1 до 9999999999999999")
-    if not (1 <= end <= 9999999999999999):
+    if not (1 <= stop <= 9999999999999999):
         raise ValueError("Конечное значение должно быть от 1 до 9999999999999999")
-    if start > end:
+    if start > stop:
         raise ValueError("Начальное значение должно быть меньше или равно конечному")
 
     # Генерируем номера карт в заданном диапазоне
-    for number in range(start, end + 1):
+    for number in range(start, stop + 1):
         formatted_number = f"{number:016d}"
         card_number = f"{formatted_number[0:4]} {formatted_number[4:8]} {formatted_number[8:12]} {formatted_number[12:16]}"
         yield card_number
